8fb87fce2974545fe0f0355b6fd82d200a844974
SPANKYWOWWOW <pavandavar99@gmail.com>
2024-10-11 12:18:47 -0400

    Initial Commit

---
+++ ScoreKeeper/ScoreKeeperApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScoreKeeperApp.swift
+//  ScoreKeeper
+//
+//  Created by Pavan Davar on 10/11/24.
+//
+
+import SwiftUI
+
+@main
+struct ScoreKeeperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
